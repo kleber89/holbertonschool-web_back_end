ebe91a2a275b9d3fbe524fcfdb70184787a67bfc
kleber89 <benkosisackson@gmail.com>
2024-07-21 17:46:38 -0500

    Methods, static methods, computed methods names..... MONEY

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,39 @@
+class Currency {
+  constructor(code, name) {
+    this._code = this._validateString(code, 'code');
+    this._name = this._validateString(name, 'name');
+  }
+
+  // Validation method
+  _validateString(value, attribute) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attribute} must be a string`);
+    }
+    return value;
+  }
+
+  // Getter and setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = this._validateString(value, 'code');
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = this._validateString(value, 'name');
+  }
+
+  // Method to display full currency
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
