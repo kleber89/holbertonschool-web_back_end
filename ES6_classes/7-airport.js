f8f26d33cecae6fb433efe346ae19092f8944c87
kleber89 <benkosisackson@gmail.com>
2024-07-21 19:03:18 -0500

    update airport

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,39 +1,10 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
-    this._name = this._validateString(name, 'name');
-    this._code = this._validateString(code, 'code');
+    this._name = name;
+    this._code = code;
   }
 
-  // Validation method
-  _validateString(value, attribute) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attribute} must be a string`);
-    }
-    return value;
-  }
-
-  // Getter and setter for name
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = this._validateString(value, 'name');
-  }
-
-  // Getter and setter for code
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    this._code = this._validateString(value, 'code');
-  }
-
-  // Override toString method
   toString() {
-    return this._code;
+    return `[object ${this._code}]`;
   }
 }
-
-export default Airport;
