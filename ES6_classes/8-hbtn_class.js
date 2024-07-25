92b9cb0a9ca9e5f4458d33e443ea58adcfea0050
kleber89 <benkosisackson@gmail.com>
2024-07-25 16:58:30 -0500

    update class

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -4,15 +4,11 @@ export default class HolbertonClass {
     this._location = location;
   }
 
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'number') {
-      return this._size;
-    }
+  valueOf() {
+    return this._size;
+  }
 
-    if (hint === 'string') {
-      return this._location;
-    }
-
-    return hint;
+  toString() {
+    return this._location;
   }
 }
