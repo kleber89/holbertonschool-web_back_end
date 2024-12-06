61a95d247cedc790030ea4d385e4e86e2e2473ef
kleber89 <benkosisackson@gmail.com>
2024-12-05 21:26:57 -0500

    annotate the following variables with the specified values

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Variables Module
+================
+
+This module defines and annotates several variables with specific values.
+
+Variables
+---------
+a : int
+    An integer with a value of 1.
+
+pi : float
+    A floating-point number representing the value of pi, with a value of 3.14.
+
+i_understand_annotations : bool
+    A boolean indicating understanding of annotations, with a value of True.
+
+school : str
+    A string with the value "Holberton".
+
+Examples
+--------
+>>> a
+1
+>>> pi
+3.14
+>>> i_understand_annotations
+True
+>>> school
+'Holberton'
+"""
+
+# Variable definitions and annotations
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
