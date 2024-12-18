84ab9db6b6ec3c186228084a311b99e3c83814bc
kleber89 <benkosisackson@gmail.com>
2024-12-17 21:57:13 -0500

    function that lists all documents in a collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""List all documents in Python"""
+
+
+def list_all(mongo_collection):
+    """ function that lists all documents in a collection"""
+    if mongo_collection is None:
+        return []
+
+    return list(mongo_collection.find())
